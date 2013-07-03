ccf68abe412866c3811600d16d9a7a6167e50d91
ilya.shaisultanov <ilya.shaisultanov@gmail.com>
2013-07-02 20:46:57 -0400

    Style
    Change-Id: I279e8f2ffc102bb2330c110a4a26de6dcfa53ea2

--- test/test.js
+++ test/test.js
@@ -1,10 +1,10 @@
 var validate = require('../')
-  , assert = require('assert');
+  , assert = require('assert')
 
 
 describe('validate', function () {
   it('Passes valid types', function () {
-    var errors;
+    var errors
 
     var schema = {
       'some string': {
@@ -57,7 +57,7 @@ describe('validate', function () {
         'type': 'boolean',
         'error': {code: 10, message: 'ten'}
       }
-    };
+    }
 
     var obj = {
       'some string': 'sdf',
@@ -70,14 +70,14 @@ describe('validate', function () {
       'some undefined': undefined,
       'some object': {},
       'some boolean': false
-    };
+    }
 
-    errors = validate(obj, schema);
-    assert.strictEqual(errors.length, 0);
-  });
+    errors = validate(obj, schema)
+    assert.strictEqual(errors.length, 0)
+  })
 
   it('Supports dot-notation', function () {
-    var errors;
+    var errors
 
     var schema = {
       'nested.property.rule': {
@@ -110,7 +110,7 @@ describe('validate', function () {
         'type': 'string',
         'error': {'code': 500, 'message': 'five hundred'}
       }
-    };
+    }
 
     var obj = {
       'nested': {
@@ -137,16 +137,16 @@ describe('validate', function () {
       'ERROR': {
         'nested': ['property']
       }
-    };
+    }
 
-    errors = validate(obj, schema);
-    assert.strictEqual(errors.length, 1);
-    assert.strictEqual(errors[0].code, 500);
-    assert.strictEqual(errors[0].message, 'five hundred');
-  });
+    errors = validate(obj, schema)
+    assert.strictEqual(errors.length, 1)
+    assert.strictEqual(errors[0].code, 500)
+    assert.strictEqual(errors[0].message, 'five hundred')
+  })
 
   it('Handles invalid property spec', function () {
-    var errors;
+    var errors
 
     var schema = {
       '.': {
@@ -159,7 +159,7 @@ describe('validate', function () {
         'type': 'string',
         'error': {'code': 2, 'message': 'two'}
       }
-    };
+    }
 
     var obj = {
       '.': {
@@ -177,18 +177,18 @@ describe('validate', function () {
           'rule': 'str'
         }
       }
-    };
+    }
 
-    errors = validate(obj, schema);
-    assert.strictEqual(errors.length, 2);
-    assert.strictEqual(errors[0].code, 1);
-    assert.strictEqual(errors[0].message, 'one');
-  });
+    errors = validate(obj, schema)
+    assert.strictEqual(errors.length, 2)
+    assert.strictEqual(errors[0].code, 1)
+    assert.strictEqual(errors[0].message, 'one')
+  })
   
   it('Inline function receives object being validated as `this`', function () {
-    var errors;
+    var errors
 
-    function isYes(prop) {return this[prop] == 'yes';}
+    function isYes(prop) {return this[prop] == 'yes'}
 
     var schema = {
       'a': {
@@ -203,28 +203,28 @@ describe('validate', function () {
         'function': isYes,
         'error': {'code': 2, 'message': 'two'}
       }
-    };
+    }
 
     var obj = {
       'a': 'yes',
       'b': 'not yes'
-    };
+    }
 
-    errors = validate(obj, schema);
-    assert.strictEqual(errors.length, 1);
-    assert.strictEqual(errors[0].code, 2);
-    assert.strictEqual(errors[0].message, 'two');
-  });
+    errors = validate(obj, schema)
+    assert.strictEqual(errors.length, 1)
+    assert.strictEqual(errors[0].code, 2)
+    assert.strictEqual(errors[0].message, 'two')
+  })
 
   it('Inline function works with optional properties', function () {
-    var errors;
+    var errors
 
     function ifAthenB(prop) {
       if (this.a) {
         if (this.b) return true
       }
       
-      return false;
+      return false
     }
 
     var schema = {
@@ -240,22 +240,22 @@ describe('validate', function () {
         'function': ifAthenB,
         'error': {'code': 2, 'message': 'two'}
       }
-    };
+    }
 
     var obj = {
       'a': 'yes'
-    };
+    }
 
-    errors = validate(obj, schema);
-    assert.strictEqual(errors.length, 2);
-    assert.strictEqual(errors[0].code, 1);
-    assert.strictEqual(errors[1].code, 2);
-    assert.strictEqual(errors[0].message, 'one');
-    assert.strictEqual(errors[1].message, 'two');
-  });
+    errors = validate(obj, schema)
+    assert.strictEqual(errors.length, 2)
+    assert.strictEqual(errors[0].code, 1)
+    assert.strictEqual(errors[1].code, 2)
+    assert.strictEqual(errors[0].message, 'one')
+    assert.strictEqual(errors[1].message, 'two')
+  })
 
   it('Supports multiple types', function () {
-    var errors;
+    var errors
 
     var schema = {
       'prop1': {
@@ -273,20 +273,20 @@ describe('validate', function () {
         'type': ['object', 'array'],
         'error': {'code': 3, 'message': 'three'}
       }
-    };
+    }
 
     errors = validate({
         'prop1': new Date()
       , 'prop2': 'string'
       , 'prop3': {}
-    }, schema);
-    assert.equal(errors.length, 0);
-  });
+    }, schema)
+    assert.equal(errors.length, 0)
+  })
   
   it('Handles function exceptions', function () {
-    var errors;
+    var errors
 
-    function isYes(prop) {return prop == iForgotToDeclareThis;}
+    function isYes(prop) {return prop == iForgotToDeclareThis}
 
     var schema = {
       'a': {
@@ -295,20 +295,20 @@ describe('validate', function () {
         'function': isYes,
         'error': {'code': 1, 'message': 'one'}
       }
-    };
+    }
 
     var obj = {
       'a': 'yes'
-    };
+    }
 
-    errors = validate(obj, schema);
-    assert.strictEqual(errors.length, 1);
-    assert.strictEqual(errors[0].code, 1);
-    assert.strictEqual(errors[0].message, 'one');
-  });
+    errors = validate(obj, schema)
+    assert.strictEqual(errors.length, 1)
+    assert.strictEqual(errors[0].code, 1)
+    assert.strictEqual(errors[0].message, 'one')
+  })
 
   it('detects invalid date', function () {
-    var errors;
+    var errors
 
     var schema = {
       'prop1': {
@@ -326,22 +326,22 @@ describe('validate', function () {
         'type': ['string', 'number', 'regexp', 'date'],
         'error': {'code': 333, 'message': 'threethreethree'}
       }
-    };
+    }
 
     errors = validate({
         'prop1': new Date('wrong!')
       , 'prop2': new Date('oops')
       , 'prop3': new Date()
-    }, schema);
-    assert.equal(errors.length, 2);
+    }, schema)
+    assert.equal(errors.length, 2)
     
     assert(errors.every(function (err) {
       return [111,222].indexOf(err.code) !== -1  
     }))
-  });
+  })
   
   it('returns error(s) for min violation', function () {
-    var errors;
+    var errors
 
     var schema = {
       'shortName': {
@@ -362,24 +362,24 @@ describe('validate', function () {
         'min': 3,
         'error': {'code': 3, 'message': 'three'}
       }
-    };
+    }
 
     var obj = {
       'shortName': '12',
       'small number': 99,
       'short array': [1,2]
-    } ;
+    } 
     
-    errors = validate(obj, schema);
-    assert.equal(errors.length, Object.keys(obj).length);
+    errors = validate(obj, schema)
+    assert.equal(errors.length, Object.keys(obj).length)
 
     assert(errors.every(function (err) {
       return [1,2,3].indexOf(err.code) !== -1
     }))    
-  });
+  })
 
   it('returns error(s) for max violation', function () {
-    var errors;
+    var errors
 
     var schema = {
       'shortName': {
@@ -400,23 +400,23 @@ describe('validate', function () {
         'max': 3,
         'error': {'code': 3, 'message': 'three'}
       }
-    };
+    }
 
     var obj = {
       'shortName': '1234',
       'small number': 101,
       'short array': [1,2,3,4]
-    } ;
+    } 
 
-    errors = validate(obj, schema);
-    assert.equal(errors.length, Object.keys(obj).length);
+    errors = validate(obj, schema)
+    assert.equal(errors.length, Object.keys(obj).length)
     assert(errors.every(function (err) {
       return [1,2,3].indexOf(err.code) !== -1
     }))
-  });
+  })
 
   it('Returns error(s) for type violation', function () {
-    var errors;
+    var errors
 
     var schema = {
       'some string': {
@@ -469,7 +469,7 @@ describe('validate', function () {
         'type': 'boolean',
         'error': {'code': 10, 'message': 'err'}
       }
-    };
+    }
 
     var obj = {
       'some string': 1,
@@ -482,17 +482,17 @@ describe('validate', function () {
       'some undefined': false,
       'some object': null,
       'some boolean': function(){}
-    };
+    }
 
-    errors = validate(obj, schema);
-    assert.strictEqual(errors.length, Object.keys(obj).length);
+    errors = validate(obj, schema)
+    assert.strictEqual(errors.length, Object.keys(obj).length)
     assert(errors.every(function (err) {
       return [1,2,3,4,5,6,7,8,9,10].indexOf(err.code) !== -1
     }))
-  });
+  })
   
   it('returns error(s) for invalid data object', function () {
-    var errors;
+    var errors
     
     var schema = {
       'startDate': {
@@ -510,65 +510,65 @@ describe('validate', function () {
         'type': ['number', 'date'],
         'errorCode': 3
       }
-    };
+    }
     
-    errors = validate(undefined, schema);
-    assert.equal(errors.length, 1);
+    errors = validate(undefined, schema)
+    assert.equal(errors.length, 1)
     assert(errors.every(function (err) {
       return ['ENODATA'].indexOf(err.code) !== -1
     }))
 
-    errors = validate(null, schema);
-    assert.equal(errors.length, 1);
+    errors = validate(null, schema)
+    assert.equal(errors.length, 1)
     assert(errors.every(function (err) {
       return ['ENODATA'].indexOf(err.code) !== -1
     }))
 
-    errors = validate({}, schema);
-    assert.equal(errors.length, 3);
+    errors = validate({}, schema)
+    assert.equal(errors.length, 3)
     assert(errors.every(function (err) {
       return ['EGENERIC'].indexOf(err.code) !== -1 &&
              /failed to validate/.test(err.message)
     }))
 
-    errors = validate([{}], schema);
-    assert.equal(errors.length, 3);
+    errors = validate([{}], schema)
+    assert.equal(errors.length, 3)
     assert(errors.every(function (err) {
       return ['EGENERIC'].indexOf(err.code) !== -1 &&
              /failed to validate/.test(err.message)
     }))
 
-    errors = validate('', schema);
-    assert.equal(errors.length, 3);
+    errors = validate('', schema)
+    assert.equal(errors.length, 3)
     assert(errors.every(function (err) {
       return ['EGENERIC'].indexOf(err.code) !== -1 &&
              /failed to validate/.test(err.message)
     }))
     
-    errors = validate(1, schema);
-    assert.equal(errors.length, 3);
+    errors = validate(1, schema)
+    assert.equal(errors.length, 3)
     assert(errors.every(function (err) {
       return ['EGENERIC'].indexOf(err.code) !== -1 &&
              /failed to validate/.test(err.message)
     }))
 
-    errors = validate(function(){}, schema);
-    assert.equal(errors.length, 3);
+    errors = validate(function(){}, schema)
+    assert.equal(errors.length, 3)
     assert(errors.every(function (err) {
       return ['EGENERIC'].indexOf(err.code) !== -1 &&
              /failed to validate/.test(err.message)
     }))
 
-    errors = validate(/a/, schema);
-    assert.equal(errors.length, 3);
+    errors = validate(/a/, schema)
+    assert.equal(errors.length, 3)
     assert(errors.every(function (err) {
       return ['EGENERIC'].indexOf(err.code) !== -1 &&
              /failed to validate/.test(err.message)
     }))
-  });
+  })
   
   it('returns error for empty type array', function () {
-    var errors;
+    var errors
 
     var schema = {
       'prop': {
@@ -576,16 +576,16 @@ describe('validate', function () {
         'type': [],
         'error': {code: 1}
       }
-    };
+    }
 
-    errors = validate({'prop': true}, schema);
-    assert.equal(errors.length, 1);
-    assert.strictEqual(errors[0].code, 1);
-    assert.strictEqual(errors[0].message, '');
-  });
+    errors = validate({'prop': true}, schema)
+    assert.equal(errors.length, 1)
+    assert.strictEqual(errors[0].code, 1)
+    assert.strictEqual(errors[0].message, '')
+  })
   
   it('ignores missing optional properties', function () {
-    var errors;
+    var errors
 
     var schema = {
       'prop': {
@@ -593,10 +593,10 @@ describe('validate', function () {
         'type': "string",
         'error': {'code': 1, 'message': 'err'}
       }
-    };
+    }
 
-    errors = validate({'not_prop': true}, schema);
-    assert.equal(errors.length, 0);
+    errors = validate({'not_prop': true}, schema)
+    assert.equal(errors.length, 0)
 
     schema = {
       'prop.more': {
@@ -604,14 +604,14 @@ describe('validate', function () {
         'type': "string",
         'error': {'code': 1, 'message': 'err'}
       }
-    };
+    }
 
-    errors = validate({'not_prop': true}, schema);
-    assert.equal(errors.length, 0);
-  });
+    errors = validate({'not_prop': true}, schema)
+    assert.equal(errors.length, 0)
+  })
 
   it('validates optional properties when present', function () {
-    var errors;
+    var errors
 
     var schema = {
       'prop': {
@@ -619,10 +619,10 @@ describe('validate', function () {
         'type': "string",
         'error': {'code': 1, 'message': 'err'}
       }
-    };
+    }
 
-    errors = validate({'prop': "string"}, schema);
-    assert.equal(errors.length, 0);
+    errors = validate({'prop': "string"}, schema)
+    assert.equal(errors.length, 0)
 
     schema = {
       'prop': {
@@ -630,10 +630,10 @@ describe('validate', function () {
         'type': "string",
         'error': {'code': 2, 'message': 'err'}
       }
-    };
+    }
 
-    errors = validate({'prop': false}, schema);
-    assert.equal(errors.length, 1);
-    assert.strictEqual(errors[0].code, 2);
-  });
-});
+    errors = validate({'prop': false}, schema)
+    assert.equal(errors.length, 1)
+    assert.strictEqual(errors[0].code, 2)
+  })
+})
